801d45f8c6b7c7c2a348f3ea13cb9aeb7e1ee5e8
Geovic Fagel <geofag@jpj.com>
2019-10-12 22:05:48 +0800

    Event Binding types

--- src/Components/EventBind.js
+++ src/Components/EventBind.js
@@ -1,27 +1,42 @@
-import React, {Component} from  'react'
+import React, {Component} from 'react';
 
-class ExtendBind extends Component {
 
+class EventBind extends Component {
     constructor(props) {
-     super(props)
-     this.state ={
-         message: "Hello"
-     }
+    super(props)
+    this.state ={
+        message: 'Bind an Event Handler'
+    }
+    // this.bindEvent= this.bindEvent.bind(this)
+    // if you will not use arrow functions
+  
     }
 
-    welcome () {
-        this.setState({
-            message:"Welcome"
-        });
-    }
+        // bindEvent() {
+        //     this.setState({
+        //         message: ' You binded an Event!'
+        //     })
+        // }
+
+
+        bindEvent = () => {
+            this.setState({
+                message:' You bind an Event'
+            })
+        }
+
+        //arrow function binding
     render() {
-        return (
+        return(
             <div>
-                <h3>{this.state.message} This is an Event Binding </h3>
-                <button onClick={() => this.welcome()}>Ok</button>
+                <h2>{this.state.message}</h2>
+                {/* <button onClick={this.bindEvent.bind(this)}>Click Here to Bind</button> */}
+                {/* <button onClick={() => this.bindEvent()}>Click Here to Bind</button> */}
+            
+                <button onClick={this.bindEvent}>Click Here to Bind</button>
             </div>
         )
     }
 }
 
-export default ExtendBind
+export default EventBind
