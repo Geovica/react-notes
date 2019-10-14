5f3e8c3a97146aae61b59bd31f9436592247310a
Geovic Fagel <geofag@jpj.com>
2019-10-15 04:02:05 +0800

    Context

--- src/Components/ComponentD.js
+++ src/Components/ComponentD.js
@@ -3,16 +3,19 @@ import React, { Component } from 'react'
 import ComponentE from './ComponentE'
 import UserContext from './userContext'
 class ComponentD extends Component {
+
+    //only works with class components
+    static contextType= UserContext
     render() {
         return (
             <div>
-              <h2>Component D content {this.context} </h2>
+              <h2>Component D contains {this.context} </h2>
                 <ComponentE />
             </div>
         )
     }
 }
 
-ComponentD.contextType = UserContext
+// ComponentD.contextType = UserContext
 
 export default ComponentD
